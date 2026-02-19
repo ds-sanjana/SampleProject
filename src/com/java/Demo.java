457566e68adeff49ae49d59075a58f07c61aaf56
Sanjana DS <sanjanads2003@gmail.com>
2026-02-19 14:49:10 +0530

    My First Commit

---
+++ src/com/java/Demo.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Demo {
+	void print(){
+		System.out.println("my first github project");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
